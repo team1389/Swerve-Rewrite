0f203c2169fb6b3a332a6c398af12a319cfbdbc7
Harry <hcodu37@gmail.com>
2022-12-08 19:14:57 -0500

    Added angular and linear velocity and acceleration constants

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,7 +9,7 @@ import edu.wpi.first.math.util.Units;
  * also swerve constants
  */
 public class RobotMap {
-    
+
     // Constants that are the same for the 4 swerve modules
     public static final class ModuleConstants {
         // Note: these are for the drive and turning motors
@@ -28,7 +28,7 @@ public class RobotMap {
 
         // Distance between right and left wheels (meters)
         public static final double ROBOT_WIDTH = 0.562991;
-        
+
         // Distance between front and back wheels (meters)
         public static final double ROBOT_LENGTH = 0.562991;
 
@@ -37,7 +37,7 @@ public class RobotMap {
                 new Translation2d(ROBOT_LENGTH / 2, ROBOT_WIDTH / 2),
                 new Translation2d(-ROBOT_LENGTH / 2, -ROBOT_WIDTH / 2),
                 new Translation2d(-ROBOT_LENGTH / 2, ROBOT_WIDTH / 2));
-        
+
         // FL is front left, BR is back right, etc.
         public static final int FL_DRIVE_PORT = 3;
         public static final int BL_DRIVE_PORT = 7;
@@ -49,7 +49,8 @@ public class RobotMap {
         public static final int FR_TURN_PORT = 6;
         public static final int BR_TURN_PORT = 10;
 
-        // Sometimes encoders are mounted backwards based on robot design, this fixes that although it's not a thing on stargazer
+        // Sometimes encoders are mounted backwards based on robot design, this fixes
+        // that although it's not a thing on stargazer
         public static final boolean FL_TURN_REVERSED = false;
         public static final boolean BL_TURN_REVERSED = false;
         public static final boolean FR_TURN_REVERSED = false;
@@ -61,7 +62,7 @@ public class RobotMap {
         public static final boolean BR_DRIVE_REVERSED = false;
 
         // Absolute encoder ports
-        public static final int FL_ABS_PORT= 11;
+        public static final int FL_ABS_PORT = 11;
         public static final int BL_ABS_PORT = 13;
         public static final int FR_ABS_PORT = 12;
         public static final int BR_ABS_PORT = 14;
@@ -71,11 +72,11 @@ public class RobotMap {
         public static final boolean FR_ABS_REVERSED = false;
         public static final boolean BR_ABS_REVERSED = false;
 
-        //The physical max if motors go full speed
-        public static final double MAX_METERS_PER_SEC = 1; //TODO: Find this
-        public static final double MAX_RADIANS_PER_SEC = 1; //TODO: Find this
+        // The physical max if motors go full speed
+        public static final double MAX_METERS_PER_SEC = 2.177; // m/s
+        public static final double MAX_RADIANS_PER_SEC = 0.785; // rad/s
 
-        public static final double MAX_LINEAR_ACCEL = 1; //TODO: Find this
-        public static final double MAX_ANGULAR_ACCEL = 1; //TODO: Find this
+        public static final double MAX_LINEAR_ACCEL = 0.381; // m/s/s
+        public static final double MAX_ANGULAR_ACCEL = 0.224; // rad/s/s
     }
 }
